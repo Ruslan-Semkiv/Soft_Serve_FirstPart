947c0cbcf361cc865c48ab7ad0f405acd9eb794c
Semkiv Ruslan <semkiv.ruslan17@gmail.com>
2021-03-10 02:20:30 +0200

    End

--- src/Sprint_4th/SixthTask.java
+++ src/Sprint_4th/SixthTask.java
@@ -2,6 +2,7 @@ package Sprint_4th;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Objects;
 
 public class SixthTask {
 }
@@ -19,6 +20,19 @@ class Person{
 
     public int getAge() {
         return age; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 
     @Override
     public String toString() {
@@ -42,6 +56,20 @@ class Employee extends Person{
     public String toString() {
         return super.toString() + ", Salary: " + salary;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), salary);
+    }
 }
 
 class Developer extends Employee{
@@ -60,40 +88,76 @@ class Developer extends Employee{
     public String toString() {
         return super.toString() + ", Level: " + level;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Developer developer = (Developer) o;
+        return level == developer.level;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), level);
+    }
 }
 enum Level{
     JUNIOR,MIDDLE,SENIOR;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
 
-class PersonComparator <Person> implements Comparator<Person> {
+class PersonComparator implements Comparator<Person> {
 
 
     @Override
     public int compare(Person o1, Person o2) {
-        return 0;
+        int index = String.CASE_INSENSITIVE_ORDER.compare(o1.name, o2.name);
+        if(index==0){
+            index = Integer.compare(o1.age,o2.age);
+        }
+        return index;
     }
 }
-class EmployeeComparator <Employee>implements Comparator<Employee>{
+class EmployeeComparator implements Comparator<Employee>{
 
     @Override
     public int compare(Employee o1, Employee o2) {
-        return 0; }
+        int index = String.CASE_INSENSITIVE_ORDER.compare(o1.name, o2.name);
+        if(index==0){
+            index = Integer.compare(o1.age, o2.age);
+            if(index==0){
+                index = Double.compare(o1.getSalary(),o2.getSalary());
+            }
+        }
+        return  index;
+         }
 }
 
-class DeveloperComparator <Developer> implements Comparator<Developer>{
-
+class DeveloperComparator implements Comparator<Developer>{
 
     @Override
     public int compare(Developer o1, Developer o2) {
-        return 0;
+        int index = String.CASE_INSENSITIVE_ORDER.compare(o1.name,o2.name);
+        if(index==0){
+            index = Integer.compare(o1.age, o2.age);
+            if(index==0){
+                index = Double.compare(o1.getSalary(), o2.getSalary());
+                if(index==0){
+                    index = o1.getLevel().compareTo(o2.getLevel());
+                }
+            }
+        }
+        return index;
     }
 }
 
 
 class Utility {
 
-    public static <T extends Person > void sortPeople(T [] array, Comparator<T> comparator){
-
+    public static <T> void sortPeople(T [] array, Comparator<T> comparator){
+        Arrays.sort(array,comparator);
     }
 }
